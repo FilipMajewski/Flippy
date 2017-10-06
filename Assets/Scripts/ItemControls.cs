1f531f9d6f04cbec90ffc2ba44ec08a2913fb724
FilipMajewski <peruwiadomo@gmail.com>
2017-10-06 16:25:49 +0100

    start

---
+++ Assets/Scripts/ItemControls.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemControls : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    Vector2 slideStart;
+    Vector2 slideStop;
+
+    public Transform raycastPosition;
+
+    public float forcePower;
+    public float torquePower;
+
+    public float rayDistance;
+
+    bool addPoints;
+    bool grounded;
+    public bool stillFliping;
+
+    public UIControls ui;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            slideStart = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            rb.isKinematic = false;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            slideStop = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            if (!stillFliping)
+            {
+                Flip();
+            }
+
+        }
+
+        if (rb.velocity.magnitude <= 0.1)
+        {
+            stillFliping = false;
+
+            if (addPoints && Physics.Raycast(raycastPosition.position, -raycastPosition.up, rayDistance))
+            {
+                AddPoints();
+                rb.isKinematic = true;
+            }
+
+            if (!Physics.Raycast(raycastPosition.position, -raycastPosition.up, rayDistance))
+            {
+                Restart();
+                ui.ResetScore();
+            }
+        }
+        else
+        {
+            stillFliping = true;
+        }
+    }
+
+    void Flip()
+    {
+
+        Vector2 move = slideStop - slideStart;
+        rb.AddForce(move * forcePower, ForceMode.Impulse);
+
+        if (move.x > 0)
+            rb.AddTorque(0, 0, -torquePower, ForceMode.Impulse);
+        else
+            rb.AddTorque(0, 0, torquePower, ForceMode.Impulse);
+
+        StartCoroutine("WaitAndPoints", 0.5f);
+    }
+
+    void AddPoints()
+    {
+        addPoints = false;
+        ui.AddScore();
+    }
+
+    void Restart()
+    {
+        Debug.Log("Restart");
+    }
+
+    private IEnumerator WaitAndPoints(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        addPoints = true;
+    }
+
+}
---
+++ Assets/Scripts/UIControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControls : MonoBehaviour
+{
+
+    public Text scoreText;
+    int score = 0;
+
+    void Start()
+    {
+        scoreText.text = score.ToString();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+    }
+
+    public void AddScore()
+    {
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+
+
+}
